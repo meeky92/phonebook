83655987c5adc2e07daa051b8ca788a7847c1655
meeky92 <112586695+meeky92@users.noreply.github.com>
2022-10-26 19:54:26 -0500

    updates

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,14 @@
 from flask import Flask
-
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-app.config.from_object(Config)
-from config import Config
+app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 from . import routes
 
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from app import app
 from app.forms import SignUpForm
 
